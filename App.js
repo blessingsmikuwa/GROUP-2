4633c7c182c1933affa8846b2c2c0f7e3cdd9b2a
bed-com-04-22 <bed-com-04-22@unima.ac.mw>
2024-11-13 09:45:09 +0200

    first commit

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import ContactDetailsForm from './ContactDetails';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <ContactDetailsForm/>
       <StatusBar style="auto" />
     </View>
   );
