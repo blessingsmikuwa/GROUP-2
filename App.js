8356a16d596a57d6da964cd595382fd3fec4bbea
bed-com-04-22 <bed-com-04-22@unima.ac.mw>
2024-11-21 23:53:13 +0200

    Created registration form

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import ContactDetailsForm from './ContactDetails';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import ContactDetailsForm from "./ContactDetails";
+import Registration from "./RegistrationForm";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <ContactDetailsForm/>
+      <Registration />
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +14,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
